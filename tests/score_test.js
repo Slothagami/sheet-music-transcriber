f1df8fefd3663d8e5dbe1c9c5a66317983dbf403
Slothagami <48915129+Slothagami@users.noreply.github.com>
2023-07-11 15:08:53 +1000

    tidy score test code

--- tests/score_test.js
+++ tests/score_test.js
@@ -1,7 +1,19 @@
-var vf, score, system, x = 0, y = 10, midiData
+var vf, score, system, midiData
+var x = 0, y = 10
+
+const staveWidth = 750
+const svgWidth   = 800
+const svgHeight  = 500
+const durations  = {
+    "w": 1,   "h": 1/2, 
+    "q": 1/4, "8": 1/8
+}
+
+window.addEventListener("load", init)
+
 function init() {
-    // MIDI data for "Megalovania"
     midiData = [
+        // Megalovania
         { keys: ["E/4"], duration: "q" },
         { keys: ["D/4"], duration: "q" },
         { keys: ["C/4"], duration: "q" },
@@ -49,70 +61,51 @@ function init() {
         { keys: ["E/4"], duration: "q" },
         { keys: ["F/4"], duration: "q" },
         { keys: ["G/4"], duration: "h" }
-    ];
-    var durations = {
-        "w": 1,   "h": 1/2, 
-        "q": 1/4, "8": 1/8
-    }
-
-    // Create a Vex.Flow.Renderer instance
+    ]
+    
+    // setup renderer and stave
     var renderer = new Vex.Flow.Renderer(
         document.getElementById("music"),
         Vex.Flow.Renderer.Backends.SVG
-    );
+    ).resize(svgWidth, svgHeight)
 
-    // Define the size of the rendering area
-    renderer.resize(500, 200);
-
-    // Create a Vex.Flow.Context instance
-    var context = renderer.getContext();
-    var stave = new Vex.Flow.Stave(10, 0, 480);
-
-    stave.addClef("treble");
-    stave.setContext(context).draw();
+    var context = renderer.getContext()
+    var stave   = new Vex.Flow.Stave(10, 0, staveWidth)
+        stave.addClef("treble")
+        stave.setContext(context).draw()
 
     // Convert MIDI data to VexFlow notes
-    var notes = [];
-    var octaveOffset = 0;
-    var bar_beats = 0
-    var bar_count = 0
-    var currentStaveY = 10
-    var staveWidth = 400
+    var notes         = []
+    var octaveOffset  = 0
+    var bar_beats     = 0
+    var bar_count     = 0
 
     for (let note of midiData) {
         note.auto_stem = true
 
         if (note.keys.length > 0) {
-            var vexNote = new Vex.Flow.StaveNote(note);
-            notes.push(vexNote);
+            var vexNote = new Vex.Flow.StaveNote(note)
+            notes.push(vexNote)
             bar_beats += durations[note.duration]
 
             if(bar_beats >= 1) {
+                // add barline
                 notes.push(new Vex.Flow.BarNote())
                 bar_beats = 0
                 bar_count++
-
-                if (bar_count >= 5) { 
-                    // bar_count = 0
-                    // currentStaveY += 100
-                    // var stave = new Vex.Flow.Stave(10, currentStaveY, staveWidth);
-                    // stave.addClef("treble").setContext(context).draw();
-                }
             }
         }
 
         if (note.isOctaveShift) {
-            octaveOffset += note.octaveShift;
+            octaveOffset += note.octaveShift
         }
     }
 
-    // Create a voice with the converted notes
-    var voice = new Vex.Flow.Voice({ num_beats: midiData.length-1, beat_value: 4 });
-    voice.addTickables(notes);
+    // create voice
+    var voice = new Vex.Flow.Voice({ num_beats: midiData.length-1, beat_value: 4 })
+    voice.addTickables(notes)
 
-    // Format and justify the notes to fit within the stave
-    var formatter = new Vex.Flow.Formatter().joinVoices([voice]).format([voice], 400);
-    voice.draw(context, stave);
+    // format notes
+    var formatter = new Vex.Flow.Formatter().joinVoices([voice]).format([voice], 400)
+    voice.draw(context, stave)
 }
-
-window.addEventListener("load", init)

c9ad60d1eac481f277d7eb07bac6a14ab30c408f
Slothagami <48915129+Slothagami@users.noreply.github.com>
2023-07-13 09:05:08 +1000

    organise files

--- scripts/settings.js
+++ scripts/settings.js
@@ -17,35 +17,26 @@ const score = [
     {keys: ["c/4","g/4"], duration: "q"},
 ]
 
-var margin, size, generator
+var margin, size, generator, elem = {}
 function init() {
     generator = new ScoreGenerator(score)
-
-    margin       = document.getElementById("margin")
-    size         = document.getElementById("size")
-    bar_margin   = document.getElementById("bmargin")
-    stave_height = document.getElementById("stave_height")
-    bars         = document.getElementById("bars")
-    clef_width   = document.getElementById("clef_width")
-
-    margin.value       = generator.settings.margin
-    size.value         = generator.settings.font_size
-    bar_margin.value   = generator.settings.bar_margin
-    stave_height.value = generator.settings.stave_height
-    bars.value         = generator.settings.row_bars
-    clef_width.value   = generator.settings.clef_width
+    
+    for(let param in generator.settings) {
+        let el = document.getElementById(param)
+        if (el) {
+            elem[param] = el
+            elem[param].value = generator.settings[param]
+        }
+    }
 
     requestAnimationFrame(update_params)
 }
 
 function update_params() {
-    generator.settings.margin        = parseFloat(margin.value)
-    generator.settings.font_size     = parseFloat(size.value)
-    generator.settings.bar_margin    = parseFloat(bar_margin.value)
-    generator.settings.stave_height  = parseFloat(stave_height.value)
-    generator.settings.row_bars      = parseFloat(bars.value)
-    generator.settings.clef_width    = parseFloat(clef_width.value)
+    for(let param in elem) {
+        generator.settings[param] = parseFloat(elem[param].value)
+    }
+    
     generator.generate()
-
     requestAnimationFrame(update_params)
 }

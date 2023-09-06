1135f2e9dad80d34a0f24fd8372cf4e0b8c768f3
Slothagami <48915129+Slothagami@users.noreply.github.com>
2023-09-07 08:55:15 +1000

    adjust scanning settings and add playback button

---
+++ scripts/scan_video.js
@@ -0,0 +1,324 @@
+// add vexflow libary, for displaying the sheet music after its generated
+//var script = document.createElement("script")
+//script.src = "https://cdn.jsdelivr.net/npm/vexflow/build/cjs/vexflow.js"
+
+class VideoParser {
+	constructor () {
+		this.video  = document.querySelector("video")
+		this.canvas = document.createElement("canvas")
+		this.c 	    = this.canvas.getContext("2d", {willReadFrequently: true})
+
+		this.canvas.width  = this.video.videoWidth
+		this.canvas.height = 15
+		this.canvas.style.position = "fixed"
+		this.canvas.style.bottom   = 0
+		this.canvas.style.right    = 0
+		
+		document.body.appendChild(this.canvas)
+
+		// constants
+		this.keywidth     = this.video.videoWidth / notes.length
+		this.min_keywidth = this.keywidth / 2
+	}
+
+}
+
+//#region Constants
+// const notes = `
+// 	A0 B0
+// 	C1 D1 E1 F1 G1 A1 B1
+// 	C2 D2 E2 F2 G2 A2 B2 
+// 	C3 D3 E3 F3 G3 A3 B3 
+// 	C4 D4 E4 F4 G4 A4 B4 
+// 	C5 D5 E5 F5 G5 A5 B5
+// 	C6 D6 E6 F6 G6 A6 B6 
+// 	C7 D7 E7 F7
+// `   .trim()
+// 	.replaceAll("\n", " ")
+// 	.replaceAll("\t", "")
+// 	.replaceAll("  ", " ")
+// 	.split(" ")
+const notes = `
+	A0 B0
+	C1 D1 E1 F1 G1 A1 B1
+	C2 D2 E2 F2 G2 A2 B2 
+	C3 D3 E3 F3 G3 A3 B3 
+	C4 D4 E4 F4 G4 A4 B4 
+	C5 D5 E5 F5 G5 A5 B5
+	C6 D6 E6 F6 G6 A6 B6 
+	C7 D7 E7 F7 G7 A7 B7 
+	C8
+`   .trim()
+	.replaceAll("\n", " ")
+	.replaceAll("\t", "")
+	.replaceAll("  ", " ")
+	.split(" ")
+
+const sharps = ["C", "D", "F", "G", "A"] // notes that have a sharp
+const fps    = 24
+//#endregion
+
+var window_y = 0
+let brightness_cutoff = .5
+let note_strength_cutoff = .55
+var resetting = false
+var capturing_video = false
+var video_speed = 1//2
+
+// Note Utility Functions
+function sharpen(note) {
+	return note.charAt(0) + "#" + note.charAt(1)
+}
+function flatten(note) {
+	return note.charAt(0) + "b" + note.charAt(1)
+}
+function note_index(note) {
+	return notes.indexOf(note)
+}
+
+var data
+function capture_data() {
+	data = parser.c.getImageData(0, window_y, parser.video.videoWidth, 1).data
+}
+
+function window_pixel(pos) {
+	// return a pixel brightness in the window
+	// arranged as [RGBA, RGBA, ...] for each pixel in order
+	pos *= 4
+
+	let r = data[pos]
+	let g = data[pos + 1]
+	let b = data[pos + 2]
+
+	return Math.max(r, g, b) / 255 // normalize brightness
+}
+
+function note_strength(note, samples=20) {
+	note = note_index(note)
+	let columnWidth = parser.video.videoWidth / notes.length
+	let start = Math.floor(note * columnWidth)
+	let stop  = Math.floor((note + 1) * columnWidth - 1)
+
+	let midpoint = (start + stop) / 2
+	let sum, lsum, rsum 
+	sum = lsum = rsum = 0	
+
+	// calculate the percentage of each side of the column that the note fills
+	for(let x = start; x < stop; x += columnWidth / samples) {
+		let sample = window_pixel(Math.floor(x)) > brightness_cutoff
+		sum += sample
+		if( x < midpoint ) {
+			lsum += sample
+		} else {
+			rsum += sample
+		}
+	}
+	return {
+		total: sum / samples, // % of column that's lit up
+		left: lsum / samples * 2,
+		right: rsum / samples * 2
+	}
+}
+
+function pressed_notes() {
+	// get the strength of every note, list the ones that pass the threshold
+	pressed = []
+	for(let note of notes) {
+		let strength = note_strength(note)
+		if(strength.total > note_strength_cutoff) {
+			pressed.push(note)
+		} else {
+			// if the left side is dark, but the right side is filled
+			if(strength.left < note_strength_cutoff &&
+				strength.right > note_strength_cutoff) {
+				// note is sharp
+				pressed.push(sharpen(note))
+			}
+
+			// if the right side is dark, but the left is filled
+			if(strength.right < note_strength_cutoff &&
+				strength.left > note_strength_cutoff) {
+				// note is flat
+				pressed.push(flatten(note))
+			}
+		}
+	}
+	return pressed
+}
+
+function draw_columns() {
+	// (for debug) draw where the calculated column positions are
+
+	// draw background for text
+	let note_name_y = 30
+	let note_name_height = 10
+	parser.c.fillStyle = "#000000aa"
+	parser.c.fillRect(0, note_name_y - note_name_height, parser.canvas.width, note_name_height * 2)
+
+	let pressed = pressed_notes()
+	for(let i = 0; i < notes.length; i++) {
+		// column highlight
+		if(pressed.includes(notes[i])) {
+			parser.c.fillStyle = "red"
+			parser.c.fillRect(parser.keywidth * i, 10, parser.keywidth, parser.canvas.width)
+		}
+		if(pressed.includes(sharpen(notes[i]))) {
+			parser.c.fillStyle = "red"
+			parser.c.fillRect(parser.keywidth * i + parser.keywidth/2, 10, parser.keywidth/2, parser.canvas.width)
+		}
+		if(pressed.includes(flatten(notes[i]))) {
+			parser.c.fillStyle = "red"
+			parser.c.fillRect(parser.keywidth * i, 10, parser.keywidth/2, parser.canvas.width)
+		}
+
+		// column line
+		parser.c.strokeStyle = "#99999922"
+		parser.c.lineWidth = .3
+		parser.c.moveTo(parser.keywidth * i, 10) // can't do to the top, because the program reads the canvas
+		parser.c.lineTo(parser.keywidth * i, parser.canvas.width)
+		parser.c.stroke()
+
+		// note label
+		parser.c.fillStyle    = "white"
+		parser.c.textAlign    = "center"
+		parser.c.textBaseline = "middle"
+		parser.c.fillText(notes[i], parser.keywidth * i + parser.keywidth, note_name_y)
+	}
+}
+
+function reset(e) {
+	// clean up events and variables
+	if(e.key == "r") {
+		document.body.removeChild(parser.canvas)
+		clearInterval(interval)
+		window.removeEventListener("keydown", reset)
+		resetting = true
+
+		console.clear()
+		console.log("reset functions")
+	}
+}
+
+function record_video() {
+	// start the video scan
+	capturing_video = true 
+	parser.video.currentTime = 0
+	parser.video.playbackRate = video_speed
+	parser.video.play()
+}
+
+function make_sheet_music(midi) {
+	// insert name and author in MIDI
+	let title = document.querySelector("#title > h1 > yt-formatted-string").innerText
+	let subtitle = document.querySelector("#text > a").innerText
+	midi = {title, subtitle, midi}
+
+	// copy the MIDI to be converted to sheet music
+	let str = JSON.stringify(midi)
+	btn.innerText = "Copy MIDI"
+	btn.onclick = () => {
+		navigator.clipboard.writeText(str)
+		btn.innerText = "Copied ✓"
+		setTimeout(() => {
+			btn.innerText = "Copy MIDI"
+		}, 3000);
+	}
+}
+
+var midi_data    = []
+var prev_notes   = []
+var active_notes = {}
+
+const ROUND_PRECISION = 10
+const round = (x, n=ROUND_PRECISION) => Math.round(x* 10**n)/10**n
+
+function update() {
+	parser.c.clearRect(0, 0, parser.canvas.width, parser.canvas.height)
+	parser.c.drawImage(parser.video, 0, 0, parser.video.videoWidth, clip_height, 0, 0, parser.video.videoWidth, clip_height)
+	capture_data()
+	//console.log(parser.video.currentTime) // in seconds
+	//if(pressed_notes().length > 0) console.log(pressed_notes())
+	// draw_columns()
+
+	if (capturing_video) {
+		let cur_notes = pressed_notes()
+
+		// if note starts, record its start time
+		for(let note of cur_notes) {
+			if(!prev_notes.includes(note)) {
+				active_notes[note] = parser.video.currentTime
+			}
+		}
+
+		// if a note ends calculate how long it was held, and add it to midi list
+		for(let note of prev_notes) {
+			if(!cur_notes.includes(note)) {
+				note_duration = parser.video.currentTime - active_notes[note]
+
+				midi_data.push({
+					"note": note,
+					"start_time": round(active_notes[note]),
+					"duration": round(note_duration)
+				})
+
+				delete active_notes[note]
+			}
+		}
+
+		prev_notes = cur_notes
+
+		// stop a few seconds before the video ends, to avoid youtube auto next
+		if(parser.video.currentTime >= parser.video.duration - 3) {
+			parser.video.pause()
+			capturing_video = false
+			make_sheet_music(midi_data)
+		}
+	}
+
+	if(!resetting) requestAnimationFrame(update)
+}
+
+var btn
+function add_button() {
+	let css = `
+		#sheet-music-button:hover {
+			background: linear-gradient(45deg, #e52d2799, #8e191c99);
+		}
+		#sheet-music-button {
+			/*background-color: #b31217;*/
+			background: linear-gradient(45deg, #e52d27, #8e191c);
+			padding: .75em 1.4em;
+			border-radius: 2em;
+			margin-left: 1.5em;
+			font-weight: 410;
+			font-family: "Roboto", "Arial", sans-serif;
+			font-size: 14px;
+			text-wrap: nowrap;
+		}
+	`
+
+	let style = document.createElement("style")
+		style.innerHTML = css
+	document.head.appendChild(style)
+
+	btn = document.createElement("div")
+	btn.id = "sheet-music-button"
+	btn.innerText = "Sheet Music"
+
+	btn.onclick = generate_sheet
+
+    let panel = document.querySelector("#owner")
+		panel.appendChild(btn)
+}
+
+var clip_height
+function generate_sheet() {
+	btn.innerText = "Scanning Video..."
+	parser = new VideoParser()
+	clip_height = parser.canvas.height
+	requestAnimationFrame(update)
+	record_video()
+	window.addEventListener("keydown", reset)
+}
+
+add_button()
--- scripts/ui.js
+++ scripts/ui.js
@@ -1,22 +1,111 @@
-window.addEventListener("load", start)
+// window.addEventListener("load", start)
 
-var notes, title, subtitle
+var notes, title, subtitle, bpm, data
 function start() {
-    navigator.clipboard.readText().then(data => {
-        data = JSON.parse(data)
-        notes = data.midi
-        title = data.title
-        subtitle = data.subtitle
-        init()
+    // navigator.clipboard.readText().then(data => {
+    //     data = JSON.parse(data)
+    //     notes = data.midi
+    //     title = data.title
+    //     subtitle = data.subtitle
+
+    //     init()
+    // })
+    data = JSON.parse(data)
+    notes = data.midi
+    title = data.title
+    subtitle = data.subtitle
+
+    init()
+}
+
+function suggest_bpm() {
+    // let counts = {}
+    // notes.forEach(note => {
+    //     let bpm = Math.round(4/note.duration)
+
+    //     if(bpm >= 30) {
+    //         counts[bpm] ??= 0
+    //         counts[bpm]++
+    //     }
+    // })
+    // return counts
+
+    let last_time = notes[0].start_time
+    let gap_total = 0
+    for(let i = 1; i < notes.length; i++) {
+        let gap = notes[i].start_time - last_time
+        gap_total += gap
+        last_time = notes[i].start_time
+    }
+    let avg_gap = gap_total / notes.length
+    return 60 / (avg_gap * 4)
+}
+
+//#region verify sound
+
+const ALL_NOTES_SHARP = [
+                                                          "", 'A0', 'A#0', 'B0',
+    'C1', 'C#1', 'D1', 'D#1', 'E1', 'F1', 'F#1', 'G1', 'G#1', 'A1', 'A#1', 'B1',
+    'C2', 'C#2', 'D2', 'D#2', 'E2', 'F2', 'F#2', 'G2', 'G#2', 'A2', 'A#2', 'B2',
+    'C3', 'C#3', 'D3', 'D#3', 'E3', 'F3', 'F#3', 'G3', 'G#3', 'A3', 'A#3', 'B3',
+    'C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4', 'A#4', 'B4',
+    'C5', 'C#5', 'D5', 'D#5', 'E5', 'F5', 'F#5', 'G5', 'G#5', 'A5', 'A#5', 'B5',
+    'C6', 'C#6', 'D6', 'D#6', 'E6', 'F6', 'F#6', 'G6', 'G#6', 'A6', 'A#6', 'B6',
+    'C7', 'C#7', 'D7', 'D#7', 'E7', 'F7', 'F#7', 'G7', 'G#7', 'A7', 'A#7', 'B7',
+    'C8'
+]
+const ALL_NOTES_FLAT = [
+                                                          "", 'A0', 'Bb0', 'B0',
+    'C1', 'Db1', 'D1', 'Eb1', 'E1', 'F1', 'Gb1', 'G1', 'Ab1', 'A1', 'Bb1', 'B1',
+    'C2', 'Db2', 'D2', 'Eb2', 'E2', 'F2', 'Gb2', 'G2', 'Ab2', 'A2', 'Bb2', 'B2',
+    'C3', 'Db3', 'D3', 'Eb3', 'E3', 'F3', 'Gb3', 'G3', 'Ab3', 'A3', 'Bb3', 'B3',
+    'C4', 'Db4', 'D4', 'Eb4', 'E4', 'F4', 'Gb4', 'G4', 'Ab4', 'A4', 'Bb4', 'B4',
+    'C5', 'Db5', 'D5', 'Eb5', 'E5', 'F5', 'Gb5', 'G5', 'Ab5', 'A5', 'Bb5', 'B5',
+    'C6', 'Db6', 'D6', 'Eb6', 'E6', 'F6', 'Gb6', 'G6', 'Ab6', 'A6', 'Bb6', 'B6',
+    'C7', 'Db7', 'D7', 'Eb7', 'E7', 'F7', 'Gb7', 'G7', 'Ab7', 'A7', 'Bb7', 'B7',
+    'C8'
+]
+
+var audioCtx
+function note_freq(note) {
+    let ind = ALL_NOTES_SHARP.indexOf(note)
+    if(ind == -1) {
+        ind = ALL_NOTES_FLAT.indexOf(note)
+    }
+
+    let freq = 2 ** ((ind - 49) / 12) * 440
+    return freq
+}
+function verify_sound() {
+    audioCtx = new(window.AudioContext || window.webkitAudioContext)()
+
+    notes.forEach(note => {
+        setTimeout(() => {play_note(note.note, note.duration*1000)}, note.start_time * 1000)
     })
 }
+function play_note(note, duration) {
+    let freq = note_freq(note)
+    
+    // create Oscillator node
+    var oscillator = audioCtx.createOscillator()
+
+    oscillator.type = 'square'
+    oscillator.frequency.value = freq
+    oscillator.connect(audioCtx.destination)
+    oscillator.start();
+
+    setTimeout(() => {
+        oscillator.stop()
+    }, duration)
+}
+//#endregion
 
 var margin, size, score, parser, elem = {}
 var params = new URLSearchParams(location.search)
 function init() {
-    
-
-    parser = new MIDIReader(notes, 168)
+    // parser = new MIDIReader(notes, 168)
+    bpm = suggest_bpm()
+    parser = new MIDIReader(notes, bpm)
     score  = new ScoreGenerator(parser.score)
 
     // load params
@@ -39,6 +128,7 @@ function init() {
     // attach button functions
     set_button("#pdf-button", score.download_pdf)
     set_button("#svg-button", score.download_svg)
+    set_button("#play-button", verify_sound)
 
     requestAnimationFrame(update_params)
 
